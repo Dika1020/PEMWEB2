aa62c1de1f8ef541acdfadaff0d88f5c73634114
INDIKA PUTRA PRAISAH <145969909+Dika1020@users.noreply.github.com>
2024-03-07 17:47:04 +0700

    Update TUGAS.php

--- pertemuan2/TUGAS.php
+++ pertemuan2/TUGAS.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form Belanja Online</title>
+    <title>Form Barang Elektronik </title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
